315b98aadb13575796bf6cd217ff5e1365dcb9e2
Yun Zi <yzi1@hawk.iit.edu>
2022-02-01 11:07:47 -0600

    feat: initialize assignment1 and gitignore

---
+++ assign1_storage_manager/storage_mgr.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "storage_mgr.h"
+#include "dberror.h"
+
+
+/* manipulating page files */
+void initStorageManager (void) {
+
+}
+
+RC createPageFile (char *fileName) {
+    return RC_OK;
+}
+
+RC openPageFile (char *fileName, SM_FileHandle *fHandle) {
+    return RC_OK;
+}
+
+RC closePageFile (SM_FileHandle *fHandle) {
+    return RC_OK;
+}
+
+RC destroyPageFile (char *fileName) {
+    return RC_OK;
+}
+
+/* reading blocks from disc */
+RC readBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage) {
+
+}
+
+int getBlockPos (SM_FileHandle *fHandle) {
+    return 0;
+}
+
+RC readFirstBlock (SM_FileHandle *fHandle, SM_PageHandle memPage) {
+    return RC_OK;
+}
+
+RC readPreviousBlock (SM_FileHandle *fHandle, SM_PageHandle memPage) {
+    return RC_OK;
+}
+
+RC readCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage) {
+    return RC_OK;
+}
+
+RC readNextBlock (SM_FileHandle *fHandle, SM_PageHandle memPage) {
+    return RC_OK;
+}
+
+RC readLastBlock (SM_FileHandle *fHandle, SM_PageHandle memPage) {
+    return RC_OK;
+}
+
+/* writing blocks to a page file */
+RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage) {
+    return RC_OK;
+}
+
+RC writeCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage) {
+    return RC_OK;
+}
+
+RC appendEmptyBlock (SM_FileHandle *fHandle) {
+    return RC_OK;
+}
+
+RC ensureCapacity (int numberOfPages, SM_FileHandle *fHandle) {
+    return RC_OK;
+}
+
